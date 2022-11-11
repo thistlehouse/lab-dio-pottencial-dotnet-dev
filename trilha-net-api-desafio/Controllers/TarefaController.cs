e04b9ef3f07829f33887f2bf94c626e75d92b95c
thistlehouse <fernando2802@gmail.com>
2022-11-10 23:47:36 -0300

    SaveChanges() para PUT e DELETE

--- trilha-net-api-desafio/Controllers/TarefaController.cs
+++ trilha-net-api-desafio/Controllers/TarefaController.cs
@@ -90,9 +90,15 @@ namespace TrilhaApiDesafio.Controllers
 
             // TODO: Atualizar as informações da variável tarefaBanco com a tarefa recebida via parâmetro
             // TODO: Atualizar a variável tarefaBanco no EF e salvar as mudanças (save changes)
-            _context.Tarefas.Update(tarefaBanco);
+            tarefaBanco.Titulo = tarefa.Titulo;
+            tarefaBanco.Descricao = tarefa.Descricao;
+            tarefaBanco.Data = tarefa.Data;
+            tarefaBanco.Status = tarefa.Status;
 
-            return Ok(nameof(Index));
+            _context.Tarefas.Update(tarefaBanco);
+            _context.SaveChanges();
+
+            return Ok();
         }
 
         [HttpDelete("{id}")]
@@ -103,8 +109,10 @@ namespace TrilhaApiDesafio.Controllers
             if (tarefaBanco == null)
                 return NotFound();
 
-            // TODO: Remover a tarefa encontrada através do EF e salvar as mudanças (save changes)            
+            // TODO: Remover a tarefa encontrada através do EF e salvar as mudanças (save changes)
             _context.Tarefas.Remove(tarefaBanco);
+            _context.SaveChanges();
+
             return NoContent();
         }
     }
